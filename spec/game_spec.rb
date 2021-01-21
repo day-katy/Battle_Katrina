c4d7dc8c44f0cab75cbb4aaba7cb3c63915c3eff
Katy Day <day.katy@gmail.com>
2021-01-21 16:49:38 +0000

    started making controllers BODY POSITIVE

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,8 +5,17 @@ describe Game do
   let(:player_1) { double }
   let(:player_2) { double }
 
-  describe '#attack' do
+  describe "#initialize" do
+    it "should start with player 1 and player 2" do
+      expect(subject.players).to eq ["Katy", "Katrina"]
+    end
+  end
 
+  # describe "#player_1" do
+  
+  # end
+
+  describe '#attack' do
     it "should attack a player" do
       expect(player_2).to receive(:take_damage)
       subject.attack(player_2)

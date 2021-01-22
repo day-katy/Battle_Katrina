1ed5d0e6bd6af1cc64c74c831430367b06c427f1
Katy Day <day.katy@gmail.com>
2021-01-22 10:50:35 +0000

    updating HTML and CSS

--- spec/features/test_infrastructure_spec.rb
+++ spec/features/test_infrastructure_spec.rb
@@ -1,6 +1,6 @@
 feature 'testing infrastructure' do
   scenario "can rup app and check page content" do
     visit('/')
-    expect(page).to have_content "Hello Katy!"
+    expect(page).to have_content "Welcome to Battle!"
   end
 end
